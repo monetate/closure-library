f43f0d3080bc98981986c96a32500538e224270f
Mike Zito <mikeyzito@gmail.com>
2018-03-19 11:26:59 -0400

    dom - add goog.dom.htmlToDocumentFragment back

--- closure/goog/dom/dom.js
+++ closure/goog/dom/dom.js
@@ -1085,6 +1085,47 @@ goog.dom.childrenToNode_ = function(doc, tempDiv) {
 
 
 /**
+ * Converts an HTML string into a document fragment.
+ * NB: Our slightly older version of closure-templates seems to generate JS calls to
+ *     goog.dom.htmlToDocumentFragment which has since been removed from goog.dom.
+ * 
+ * @param {string} htmlString The HTML string to convert.
+ * @return {!Node} The resulting document fragment.
+ */
+goog.dom.htmlToDocumentFragment = function (htmlString) {
+  return goog.dom.htmlToDocumentFragment_(document, htmlString);
+};
+
+
+/**
+ * Helper for {@code htmlToDocumentFragment}.
+ *
+ * @param {!Document} doc The document.
+ * @param {string} htmlString The HTML string to convert.
+ * @return {!Node} The resulting document fragment.
+ * @private
+ */
+goog.dom.htmlToDocumentFragment_ = function (doc, htmlString) {
+  var tempDiv = doc.createElement('div');
+  if (goog.dom.BrowserFeature.INNER_HTML_NEEDS_SCOPED_ELEMENT) {
+    tempDiv.innerHTML = '<br>' + htmlString;
+    tempDiv.removeChild(tempDiv.firstChild);
+  } else {
+    tempDiv.innerHTML = htmlString;
+  }
+  if (tempDiv.childNodes.length == 1) {
+    return /** @type {!Node} */ (tempDiv.removeChild(tempDiv.firstChild));
+  } else {
+    var fragment = doc.createDocumentFragment();
+    while (tempDiv.firstChild) {
+      fragment.appendChild(tempDiv.firstChild);
+    }
+    return fragment;
+  }
+};
+
+
+/**
  * Returns true if the browser is in "CSS1-compatible" (standards-compliant)
  * mode, false otherwise.
  * @return {boolean} True if in CSS1-compatible mode.
